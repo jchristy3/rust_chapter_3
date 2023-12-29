c0a6b5a8a6c675fb6071f26010d71f669068eebc
John Christy <christyjohn14@gmail.com>
2023-12-28 19:44:32 -0500

    Update main.rs
    array section

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
+use std::io;
+
 fn main() {
-    let x = 5_000;
+    let a = [1, 2, 3, 4, 5];
 
-    let x = x + 1;
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is {x}");
+    loop {
+
+        println!("Please enter an array index.");
+
+        let mut index = String::new();
+
+        io::stdin().read_line(&mut index)
+            .expect("Failed to read line");
+
+        let index: usize = index
+            .trim()
+            .parse()
+            .expect("Index entered was not a number");
+
+        if index >= a.len() {
+            println!("Index {} is out of bounds", index);
+            continue;
+        } else {
+            println!("The value of the element at index {} is: {}", index, a[index]);
+            break;
+        }
+
     }
-
-    println!("The value of x is {x}");
 }
