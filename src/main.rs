b0863122d8a3283db5738740240ee64eb866a948
John Christy <christyjohn14@gmail.com>
2024-01-05 16:55:13 -0500

    Update main.rs
    use of for loop

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,11 @@
 use std::io;
 
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+  let mut number = 3;
 
-    loop {
+  for number in (1..4).rev() {
+    println!("{}!", number);
+  }
 
-        println!("Please enter an array index.");
-
-        let mut index = String::new();
-
-        io::stdin().read_line(&mut index)
-            .expect("Failed to read line");
-
-        let index: usize = index
-            .trim()
-            .parse()
-            .expect("Index entered was not a number");
-
-        if index >= a.len() {
-            println!("Index {} is out of bounds", index);
-            continue;
-        } else {
-            println!("The value of the element at index {} is: {}", index, a[index]);
-            break;
-        }
-
-    }
+  println!("LIFTOFF!!!");
 }
